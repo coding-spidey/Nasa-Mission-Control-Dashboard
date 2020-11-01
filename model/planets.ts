2391023f5ec1c89deb7f446a0a93c87e53f56b68
coding-spidey <coding-spidey@users.noreply.github.com>
2020-11-01 12:56:06 +0530

    First Commit

---
+++ model/planets.ts
@@ -0,0 +1,61 @@
+// Importing Standard library modules
+import { join } from "../deps.ts";
+import { parse } from "../deps.ts";
+import { BufReader } from "../deps.ts";
+
+// Importing Third party modules
+import { pick } from "../deps.ts";
+
+interface Planet {
+  [key: string]: string;
+}
+
+let planets: Array<Planet>;
+
+export function filterHabitablePlanets(planets: Array<Planet>) {
+  return planets.filter((planet) => {
+    const planetaryRadius = Number(planet["koi_prad"]);
+    const stellarRadius = Number(planet["koi_srad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+
+    return planet["koi_disposition"] === "CONFIRMED" &&
+      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
+      stellarRadius > 0.99 && stellarRadius < 1.01 &&
+      stellarMass > 0.78 && stellarMass < 1.04;
+  });
+}
+
+async function loadPlanetData() {
+  const path = join("Data", "kepler_exoplanets_nasa.csv");
+
+  const file = await Deno.open(path);
+  const bufReader = new BufReader(file);
+
+  const result = await parse(bufReader, {
+    skipFirstRow: true,
+    comment: "#",
+  });
+
+  // Close file resource id (rid) to avoid leaking resources.
+  Deno.close(file.rid);
+
+  const planets = filterHabitablePlanets(result as Array<Planet>);
+
+  return planets.map((planet) => {
+    return pick(planet, [
+      "kepler_name",
+      "koi_prad",
+      "koi_smass",
+      "koi_srad",
+      "koi_count",
+      "koi_steff",
+      "koi_period",
+    ]);
+  });
+}
+
+planets = await loadPlanetData();
+
+export function getPlanetData() {
+  return planets;
+}
